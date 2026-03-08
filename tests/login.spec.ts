1cabcbc66130970bf15ec6cde193c693115c5fe0
Caroline Zhou <caroline.yuan.zhou@gmail.com>
2026-03-07 19:13:48 -0700

    added 2 simple login tests

---
+++ tests/login.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import { users } from "../utils/login_test_data";
+
+test("login with valid credential", async({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await loginPage.login(users.standard.username, users.standard.password);
+
+  await expect(page).toHaveURL(/inventory/);
+});
+
+test("login with invalid credential", async({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await loginPage.login(users.invalid.username, users.invalid.password);
+
+  await expect(page.locator("[data-test='error']")).toBeVisible();
+});
+
